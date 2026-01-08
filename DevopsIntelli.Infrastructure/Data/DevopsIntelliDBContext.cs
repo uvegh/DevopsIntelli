dbedeea2652db405b8ac9cbf0ebbc34b3bb11e76
eghobamien vincent <uveghobamien@gmail.com>
2026-01-08 19:11:15 +0000

    Introduce EF Core DBContext and domain event interceptor
    Replaced old DBContext with DevopsIntelliDBContext using EF Core and added Incident DbSet. Integrated MediatR for domain event handling and added DomainEventInterceptor to publish and clear domain events on save. Updated project file to include MediatR package reference.

---
+++ DevopsIntelli.Infrastructure/Data/DevopsIntelliDBContext.cs
@@ -0,0 +1,16 @@
+﻿using DevopsIntelli.Domain.Common.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevopsIntelli.Infrastructure.Data;
+
+public class DevopsIntelliDBContext : DbContext
+{
+
+    public DevopsIntelliDBContext(DbContextOptions<DevopsIntelliDBContext> options) : base(options){ }
+   public DbSet<Incident> Incident { get; set; }
+
+   
+}
---
+++ DevopsIntelli.Infrastructure/Persistence/DomainEventInterceptor.cs
@@ -0,0 +1,49 @@
+﻿using DevopsIntelli.Domain.Common.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+
+namespace DevopsIntelli.Infrastructure.Persistence
+{
+    public  class DomainEventInterceptor:SaveChangesInterceptor
+    {
+        private readonly IPublisher _publisher;
+
+        public DomainEventInterceptor(IPublisher publisher)
+        {
+            _publisher = publisher;
+        }
+
+        
+        public override async ValueTask<InterceptionResult<int>> SavingChangesAsync (DbContextEventData eventData,
+            InterceptionResult<int> result,
+            CancellationToken cancellationToken =default,
+            CancellationToken ct =default
+            )
+        {
+            await _publisher.Publish(eventData);
+            return result;
+        }
+
+        private async Task PublishDomainEvents(DbContext context, CancellationToken ct)
+        {
+            if (context == null)
+                return;
+            var entities = context.ChangeTracker.Entries<BaseEntity>()
+                //where theres any domainevent
+                .Where(e => e.Entity.DomainEvents.Any())
+                //seelect entity then
+                .Select(e => e.Entity).ToList();
+
+            var domainEvents = entities.SelectMany(e => e.DomainEvents).ToList();
+            //clear domain events after that
+            entities.ForEach(e => e.ClearDomainEvent());
+            
+            foreach(var domainEvent in domainEvents)
+            {
+                await _publisher.Publish(domainEvent,ct);
+            }
+        }
+    }
+}
