17f26c3b77d298b1a7d069a3f585337b1f1e555d
eghobamien vincent <uveghobamien@gmail.com>
2026-01-17 21:06:00 +0000

    implement redis cacheserivce interfaces operations

--- DevopsIntelli.Infrastructure/Caching/DistributedLockFactory.cs
+++ DevopsIntelli.Infrastructure/Caching/DistributedLockFactory.cs
@@ -1,23 +1,64 @@
 ﻿
 
+
+
+using DevopsIntelli.Application.common.Interface;
 using RedLockNet;
 using RedLockNet.SERedis;
 using RedLockNet.SERedis.Configuration;
 
-namespace DevopsIntelli.Infrastructure.Caching;
 
-public class DistributedLockFactory:IDistributedLockFactory
+namespace DevopsIntelli.Infrastructure.Caching;
+//using redlock to prevent multiple servers from processing the same task simultaneously
+public class DistributedLockFactory:IDistributedLockFactoryLocal
 {
     private readonly RedLockFactory _redlockFactory;
     public DistributedLockFactory(RedisConnectionService redisConnection)
     {
         //create multiplexer- mulitple parallel connections
-        var multiplexer = new List<RedLockMultiplexer>
+        var multiplexers = new List<RedLockMultiplexer>
         {
             redisConnection.Connection
 
         };
-        
+        _redlockFactory = RedLockFactory.Create(multiplexers);
+    }
 
+
+
+    public async Task<IDistributedLock> CreateLockAsync(
+        string resource, TimeSpan expiryTime, CancellationToken ct = default)
+    {
+        var redlock = await _redlockFactory.CreateLockAsync(resource, expiryTime,
+            waitTime: TimeSpan.FromSeconds(10),//wait 10sec to acquire lock
+            retryTime: TimeSpan.FromMilliseconds(100),// if failed retry every 100ms
+            ct
+            );
+        return new DistributedLockWrapper(redlock);
+
+    }
+
+    //shuts down entire factory
+    public void Dispose()
+    {
+        _redlockFactory.Dispose();
+    }
+
+    internal class DistributedLockWrapper : IDistributedLock
+    {
+        private readonly IRedLock _redLock;
+        public DistributedLockWrapper( IRedLock redLock)
+        {
+            _redLock = redLock;
+        }
+        bool IsAcquired => _redLock.IsAcquired;
+
+        bool IDistributedLock.IsAcquired => IsAcquired;
+
+        public async ValueTask DisposeAsync()
+        {
+            //releases the lock in redis automatically, this is for cleanup-this dispose is for lock cleanup
+          await  _redLock.DisposeAsync();
+        }
     }
 }
---
+++ DevopsIntelli.Infrastructure/Caching/RedisCacheService.cs
@@ -0,0 +1,83 @@
+﻿using DevopsIntelli.Application.common.Interface;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+using System.Text.Json;
+
+namespace DevopsIntelli.Infrastructure.Caching;
+
+public class RedisCacheService : ICacheService
+{
+
+    private readonly IDatabase _database;
+    private readonly ILogger<RedisCacheService> _logger;
+    private readonly JsonSerializerOptions _jsonOptions = new()
+    {
+        PropertyNameCaseInsensitive=true,
+        WriteIndented=false
+    };
+
+    public RedisCacheService(RedisConnectionService redisConnectionService, ILogger<RedisCacheService> logger)
+    {
+      _database=  redisConnectionService.Database;
+
+    }
+    public async Task<bool> ExistsAsync(string key, CancellationToken cancellationToken = default)
+    {
+        //var value = await _database.StringGetAsync(key);
+
+        //return !value.IsNullOrEmpty;
+      return  await _database.KeyExistsAsync(key);
+    }
+
+    public async Task<T?> GetAsync<T>(string key, CancellationToken cancellationToken = default) where T : class
+    {
+        try
+        {
+            var value = await _database.StringGetAsync(key);
+            if (value.IsNullOrEmpty)
+            {
+                _logger.LogDebug("Cache key does not exist,{key}", key);
+
+                return null;
+            }
+           
+            return JsonSerializer.Deserialize<T>(value.ToString()!, _jsonOptions);
+
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("Error getting value for {ex}", ex);
+            return null;
+
+        }
+    }
+
+    public async Task RemoveAsync(string key, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            await _database.KeyDeleteAsync(key);
+            _logger.LogDebug("remove cache key :{key}", key);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex,"Failed to remove key: {key}", key);
+
+        }
+    }
+
+    public async Task SetAsync<T>(string key, T value, TimeSpan expiration, CancellationToken cancellationToken = default) where T : class
+    {
+        try
+        {
+           var res=  JsonSerializer.Serialize<T>(value,_jsonOptions);
+           await _database.StringSetAsync(key,res,expiration);
+            _logger.LogDebug("key :{key} cached for  timespan:{expiration}", key, expiration);
+
+        }catch(Exception err)
+        {
+            _logger.LogError(err, "failed to set value for {key}", key);
+            
+        }
+    }
+}
