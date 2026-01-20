47b4f55c51b11d77bed9d6198def98bfc0bf5628
eghobamien vincent <uveghobamien@gmail.com>
2026-01-20 00:20:30 +0000

    add custome exception to global exception handler and new ProblemDetails instance

---
+++ DevopsIntelli.Application/common/Exceptions/NotFoundException.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace DevopsIntelli.Application.common.Exceptions;
+
+public class NotFoundException:Exception
+{
+    public string EntityName;
+    public Object Key;
+    public NotFoundException(string entityName, object key) : base($"{entityName} with {key} not found")
+    {
+        EntityName = entityName;
+        Key = key;
+
+    }
+}
---
+++ DevopsIntelli.Application/common/Exceptions/ValidationException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevopsIntelli.Application.common.Exceptions;
+
+
+public class ValidationException:Exception
+{
+    /// <summary>
+    /// define error property
+    /// </summary>
+    public IDictionary<string, string[]> Errors { get; }
+    public string Entity;
+    public object Name;
+
+
+
+    public ValidationException(IDictionary<string, string[]> error) : base("One or more validation errors occured") {
+        Errors = error;
+    
+    }
+    
+
+    
+}
