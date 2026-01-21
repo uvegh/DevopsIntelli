c235ef8e704d3519ea9e40ed685654a206a73b4d
eghobamien vincent <uveghobamien@gmail.com>
2026-01-21 20:48:01 +0000

    create embedding interface and setup ollama embedding generation

---
+++ DevopsIntelli.Application/common/Interface/IEmbeddingService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevopsIntelli.Application.common.Interface;
+
+public interface IEmbeddingService
+{
+    Task<float[]> GenerateEmbeddingAsync(string text, CancellationToken ct = default);
+}
