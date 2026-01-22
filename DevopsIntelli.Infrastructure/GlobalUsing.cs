84b802b51f32b62ead73f47e1fffa45705e78340
eghobamien vincent <uveghobamien@gmail.com>
2026-01-22 17:03:07 +0000

    global using for infras project

---
+++ DevopsIntelli.Infrastructure/GlobalUsing.cs
@@ -0,0 +1,5 @@
+﻿
+global using DevopsIntelli.Application.common.Interface;
+global using DevopsIntelli.Application.DTO;
+global using Microsoft.Extensions.Logging;
+global using System.Net.Http.Json;
