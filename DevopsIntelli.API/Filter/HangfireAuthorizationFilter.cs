08682952512b88a45f84826231b0a712bff7b20d
eghobamien vincent <uveghobamien@gmail.com>
2026-01-25 02:17:21 +0000

    Hangfire setup

---
+++ DevopsIntelli.API/Filter/HangfireAuthorizationFilter.cs
@@ -0,0 +1,17 @@
+﻿using Hangfire.Annotations;
+using Hangfire.Dashboard;
+
+namespace DevopsIntelli.API.Filter;
+
+public class HangfireAuthorizationFilter : IDashboardAuthorizationFilter
+{
+    public bool Authorize([NotNull] DashboardContext context)
+    {
+
+        // for prod:
+        // var httpContext = context.GetHttpContext();
+        // return httpContext.User.Identity?.IsAuthenticated == true &&
+        //        httpContext.User.IsInRole("Admin");
+        return true;
+    }
+}
