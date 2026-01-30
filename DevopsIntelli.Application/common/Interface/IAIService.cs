6a36d98623e8073cded7c6cd7525390116d3f414
eghobamien vincent <uveghobamien@gmail.com>
2026-01-30 02:01:38 +0000

    Change AnalyzeIncidentAsync return type to IncidentAnalysisDTO
    Updated the IAIService interface so that AnalyzeIncidentAsync now returns an IncidentAnalysisDTO instead of IncidentAnalysis. Implementations should be updated accordingly.

--- DevopsIntelli.Application/common/Interface/IAIService.cs
+++ DevopsIntelli.Application/common/Interface/IAIService.cs
@@ -12,6 +12,6 @@ public interface  IAIService
     /// <param name="ct">A cancellation token that can be used to cancel the analysis operation.</param>
     /// <returns>A task that represents the asynchronous operation. The task result contains an <see cref="IncidentAnalysis"/>
     /// object with details about the analyzed incident.</returns>
-    Task<IncidentAnalysis> AnalyzeIncidentAsync(Guid incidentId, CancellationToken ct=default);
+    Task<IncidentAnalysisDTO> AnalyzeIncidentAsync(Guid incidentId, CancellationToken ct=default);
     Task<LogAnalysisResult> AnalyzeLogsAsync(string logContent, CancellationToken ct = default);
 }
